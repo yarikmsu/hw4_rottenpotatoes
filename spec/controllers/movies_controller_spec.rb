fca5e44383b07e589198f8ddbfff60b5fdb5829f
Yaroslav Gorbunov <yagorbunov@mail.ru>
2012-03-26 03:13:42 +0400

    update and destroy examples

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -6,9 +6,34 @@ describe MoviesController do
     it 'should make the movie attributes available to that template for editing'
   end
   describe 'update movie info' do
-    it 'should call update_attributes! method'
-    it 'should notice about updating the movie'
-    it 'should redirect to the movie page'
+    before(:each) do
+      @movie = {"title"=>"s", "rating"=>"G", "release_date(1i)"=>"2012", "release_date(2i)"=>"3", "release_date(3i)"=>"25"}
+      @m = mock('movie', :id => '1234', :title => 'Alien')
+    end
+    it 'should call find method' do
+      Movie.should_receive(:find).with('1234').and_return(@m)
+      @m.stub(:update_attributes!)
+      put :update, :id => 1234, :movie => @movie
+    end
+    it 'should call update_attributes! method' do
+      Movie.stub(:find).with('1234').and_return(@m)
+      @m.should_receive(:update_attributes!).with(@movie)
+      put :update, :id => 1234, :movie => @movie
+    end
+    describe 'after update_attributes!' do
+      before(:each) do
+        Movie.stub(:find).with('1234').and_return(@m)
+        @m.stub(:update_attributes!).with(@movie)
+        put :update, :id => 1234, :movie => @movie
+      end
+      it 'should notice about updating the movie' do
+        @controller.instance_eval{flash.stub!(:sweep)}
+        flash.now[:notice].should be == "Alien was successfully updated."
+      end
+      it 'should redirect to the movie page' do
+        response.should redirect_to(:action=>'show', :controller=>'movies', :id=>@m)
+      end
+    end
   end
   describe 'show movie info' do
     it 'should select show template for rendering'
@@ -44,33 +69,38 @@ describe MoviesController do
     end
   end
   describe 'destroy the movie' do
-    it 'should call destroy method'
-    it 'should notice about destroying the movie'
-    it 'should redirect to the movies list'
+    before(:each) do
+      # @movie = {"title"=>"s", "rating"=>"G", "release_date(1i)"=>"2012", "release_date(2i)"=>"3", "release_date(3i)"=>"25"}
+      @m = mock('movie', :id => '1234', :title => 'Alien')
+    end
+    it 'should call find method' do
+      Movie.should_receive(:find).with('1234').and_return(@m)
+      @m.stub(:destroy)
+      delete :destroy, :id => 1234
+    end
+    it 'should call destroy method' do
+      Movie.stub(:find).with('1234').and_return(@m)
+      @m.should_receive(:destroy)
+      delete :destroy, :id => 1234
+    end
+    context 'after destroy' do
+      before(:each) do
+        Movie.stub(:find).with('1234').and_return(@m)
+        @m.stub(:destroy)
+        delete :destroy, :id => 1234
+      end
+      it 'should notice about destroying the movie' do
+        @controller.instance_eval{flash.stub!(:sweep)}
+        flash.now[:notice].should be == "Movie 'Alien' deleted."
+      end
+      it 'should redirect to the movies list' do
+        response.should redirect_to(:movies)
+      end
+    end
   end
   describe 'index' do
     it 'should call find_all_by_rating method'
     it 'should select index template for rendering'
     it 'should make the movies list available to that template for editing'
-  end
-      
-  describe 'Update Movie Info2' do
-    # before(:each) do
-      # post :create, {:title => 'Alien', :rating => 'R', :release_date => '1979-05-25'}
-    # end
-    # it 'should call update method' do
-      # @movie = Movie.find(1)
-      # put :update, {:id => 1, :movie => {:title => 'Alien', :rating => 'R', :release_date => '1979-05-25', :director => 'Mike'}}
-      # @movie.should_receive(:update).with(anything())
-      # response.should redirect_to(:controller => 'movies', :action => 'show')
-      # get :index
-      # post :create
-      # get :new
-      # get :edit, {:id => 1}
-      # debugger
-      # p Movie.find_by_title('Alien')
-      # id = Movie.find_by_title('Alien')[:id]
-      # get :show, {:id => id}
-    # end
   end
 end
